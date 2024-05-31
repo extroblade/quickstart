d4b4ac606363fba09ff1400493a082a72b5fc3e5
Alexey Cherepanov <cherepanov.a@future-group.ru>
2024-05-31 15:31:51 +0400

    feat: add test and build job

--- src/app/tests/inspector.spec.ts
+++ src/app/tests/inspector.spec.ts
@@ -10,16 +10,4 @@ test("test", async ({ page }) => {
   await page.getByPlaceholder("What needs to be done?").fill("tsestw2");
   await page.getByTestId("todo-title").click();
   await page.getByLabel("Toggle Todo").check();
-  await page.getByPlaceholder("What needs to be done?").click();
-  await page.getByPlaceholder("What needs to be done?").click();
-  await page.getByPlaceholder("What needs to be done?").press("Enter");
-  await page.getByText("tsestw2").click();
-  await page.getByRole("textbox", { name: "Edit" }).fill("edited");
-  await page.getByRole("textbox", { name: "Edit" }).press("Enter");
-  await page
-    .getByText(
-      "This is just a demo of TodoMVC for testing, not the real TodoMVC app. todosMark",
-    )
-    .click();
-  await page.getByRole("button", { name: "Clear completed" }).click();
 });
