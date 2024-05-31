498e7f79e14642bbd82a1c45b13a57b073f9a13d
Alexey Cherepanov <cherepanov.a@future-group.ru>
2024-05-31 15:31:09 +0400

    fix: exclude pw from unit tests

--- jest.config.ts
+++ jest.config.ts
@@ -9,6 +9,10 @@ const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "<rootDir>/src/app/tests/", // Exclude Playwright tests directory
+  ],
 };
 
 export default createJestConfig(config);
