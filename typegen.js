067f316cc7a5a03bd90376efc962aaad3e991cf2
Alexey Cherepanov <cherepanov.a@future-group.ru>
2024-06-03 14:55:46 +0400

    fix: typegen

--- typegen.js
+++ typegen.js
@@ -12,7 +12,7 @@ if (!API_URL) {
 
 const swaggerFilePath = path.resolve(__dirname, "src/shared/types/schema.yaml");
 const curlCommand = `curl ${API_URL} ${process.env.API_USER && process.env.API_PASS ? `--user ${process.env.API_USER}:${process.env.API_PASS}` : ""} -o ${swaggerFilePath}`;
-
+console.log(curlCommand);
 try {
   execSync(curlCommand, { stdio: "inherit" });
 
